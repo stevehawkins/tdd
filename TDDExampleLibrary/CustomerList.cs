c4e57f023ec5d8e103e59e5dc8ba14b5a29f405e
stevehawkins <steve@steve-hawkins.com>
2017-02-22 22:46:30 +0000

    Add files via upload
    example source Stage 1

---
+++ TDDExampleLibrary/CustomerList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExampleLibrary
+{
+
+    public class CustomerList
+    {
+        private List<Tuple<string, float, enumCategories>> _menuList = new List<Tuple<string, float, enumCategories>>
+        {
+            new Tuple<string, float, enumCategories>("cola", 0.5f, enumCategories.COLD),
+            new Tuple<string, float, enumCategories>("coffee", 1.0f, enumCategories.HOT),
+            new Tuple<string, float, enumCategories>("cheese sandwich", 2.0f, enumCategories.COLD),
+            new Tuple<string, float, enumCategories>("steak sandwich", 4.5f, enumCategories.HOT)
+        };
+
+        private List<float> _menuCost = new List<float>();
+        public CustomerList()
+        {
+        }
+
+        public void Add(string item)
+        {
+            var itm = _menuList.Where(x => x.Item1 == item).FirstOrDefault();
+
+            if (itm != null)
+                _menuCost.Add(itm.Item2);
+        }
+
+        public string GetBill()
+        {
+            var ret = string.Empty;
+            var total = _menuCost.Sum();
+            return total.ToString("n2");
+        }
+    }
+}
---
+++ TDDExampleTests/TestRunner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDExampleLibrary;
+
+namespace TDDExampleTests
+{
+    [TestClass]
+    public class TestRunner
+    {
+        public object CustomerList { get; private set; }
+
+        [TestMethod]
+        public void AddItem_CoffeeReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            Assert.AreEqual("1.00", bill.GetBill());
+        }
+
+        [TestMethod]
+        public void AddItem_CoffeeNoReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffeeeeeeeee");
+            Assert.AreNotEqual("1.00", bill.GetBill());
+        }
+
+        [TestMethod]
+        public void AddItem_MenuListReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+            bill.Add("cheese sandwich");
+            Assert.AreEqual("3.50", bill.GetBill());
+        }
+    }
+}
