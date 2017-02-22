6cb88e4c7346599b24ce2e2ade497d001f02758a
stevehawkins <steve@steve-hawkins.com>
2017-02-22 23:03:59 +0000

    Add files via upload
    stage 2 complete

---
+++ CustomerList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExampleLibrary
+{
+
+    public class CustomerList
+    {
+        private List<Tuple<string, float, enumCategories>> _menuList = new List<Tuple<string, float, enumCategories>>
+        {
+            new Tuple<string, float, enumCategories>("cola", 0.5f, enumCategories.DRINK),
+            new Tuple<string, float, enumCategories>("coffee", 1.0f, enumCategories.DRINK),
+            new Tuple<string, float, enumCategories>("cheese sandwich", 2.0f, enumCategories.COLDFOOD),
+            new Tuple<string, float, enumCategories>("steak sandwich", 4.5f, enumCategories.HOTFOOD)
+        };
+
+        private List<Tuple<string, float, enumCategories>> _menuCost = new List<Tuple<string, float, enumCategories>>();
+        public CustomerList()
+        {
+        }
+
+        public void Add(string item)
+        {
+            var itm = _menuList.Where(x => x.Item1 == item).FirstOrDefault();
+
+            if (itm != null)
+                _menuCost.Add(itm);
+        }
+
+        public string GetBill()
+        {
+            var ret = string.Empty;
+            var total = _menuCost.Sum(x=>x.Item2);
+            return total.ToString("n2");
+        }
+
+        public string GetServiceCost()
+        {
+            var ret = string.Empty;
+            // 10% charge 
+            var coldFood = (_menuCost.Where(p=>p.Item3==enumCategories.COLDFOOD).Sum(x => x.Item2))*0.1;
+            // 20% charge
+            var hotFood = (_menuCost.Where(p => p.Item3 == enumCategories.HOTFOOD).Sum(x => x.Item2)) * 0.2;
+            if (hotFood > 20.0f) hotFood = 20.0f;
+            return (hotFood+coldFood).ToString("n2");
+        }
+    }
+}
---
+++ TestRunner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDExampleLibrary;
+
+namespace TDDExampleTests
+{
+    [TestClass]
+    public class TestRunner
+    {
+        public object CustomerList { get; private set; }
+
+        [TestMethod]
+        public void AddItem_CoffeeReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            Assert.AreEqual("1.00", bill.GetBill());
+        }
+
+        [TestMethod]
+        public void AddItem_CoffeeNoReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffeeeeeeeee");
+            Assert.AreNotEqual("1.00", bill.GetBill());
+        }
+
+        [TestMethod]
+        public void AddItem_MenuListReturnValue()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+            bill.Add("cheese sandwich");
+            Assert.AreEqual("3.50", bill.GetBill());
+        }
+
+        [TestMethod]
+        public void AddItem_ServiceChargeColdFood()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+            bill.Add("cheese sandwich");
+
+            Assert.AreEqual("0.20", bill.GetServiceCost());
+        }
+
+        [TestMethod]
+        public void AddItem_NoServiceCharge()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+
+            Assert.AreEqual("0.00", bill.GetServiceCost());
+        }
+
+        [TestMethod]
+        public void AddItem_ServiceChargeHotFood()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+            bill.Add("steak sandwich");
+
+            Assert.AreEqual("0.90", bill.GetServiceCost());
+        }
+
+        [TestMethod]
+        public void AddItem_ServiceChargeHotFoodOverMaxCharge()
+        {
+            var bill = new CustomerList();
+            bill.Add("coffee");
+            bill.Add("cola");
+
+            for (int x = 0; x < 100; x++)
+            {
+                bill.Add("steak sandwich");
+            }
+
+            Assert.AreEqual("20.00", bill.GetServiceCost());
+        }
+
+    }
+}
